52f0b31a49cf7c4490db63f08d351361e0453bc0
sahil <sahil_mohammad@outlook.com>
2020-02-17 20:00:20 +0400

    WebServiceConfig

---
+++ soap-course-management/src/main/java/com/orbiter/soap/webservices/soapcoursemanagement/soap/WebServiceConfig.java
@@ -0,0 +1,46 @@
+package com.orbiter.soap.webservices.soapcoursemanagement.soap;
+
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.ws.config.annotation.EnableWs;
+import org.springframework.ws.transport.http.MessageDispatcherServlet;
+import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
+import org.springframework.xml.xsd.SimpleXsdSchema;
+import org.springframework.xml.xsd.XsdSchema;
+
+@EnableWs
+//Spring configuration
+@Configuration
+public class WebServiceConfig {
+//MessageDispatcherServlet
+	
+	@Bean
+	public ServletRegistrationBean messageDispatcherServlet(ApplicationContext context) {
+		MessageDispatcherServlet messageDispatcherServlet = new MessageDispatcherServlet();
+		messageDispatcherServlet.setApplicationContext(context);
+		messageDispatcherServlet.setTransformWsdlLocations(true);
+		return new ServletRegistrationBean(messageDispatcherServlet , "/ws");
+	}
+	
+	@Bean(name="courses")
+	public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema courseSchema) {
+			DefaultWsdl11Definition definition = new DefaultWsdl11Definition();
+			//PortType - CoursePort
+			//Namespace - http://orbiter.com/courses
+			definition.setPortTypeName("CoursePort");
+			definition.setTargetNamespace("http://orbiter.com/courses");
+			definition.setLocationUri("/ws");
+			definition.setSchema(courseSchema);
+	return definition;
+	}
+	
+	@Bean
+	public XsdSchema courseSchema() {
+		return new SimpleXsdSchema(new ClassPathResource("course-details.xsd"));	
+	}
+
+	
+}
